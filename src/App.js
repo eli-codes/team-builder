727d1a87fec0e20316db6b097c023a4260e34d0a
Elizabeth Tutor <elitutor0116@gmail.com>
2023-02-09 13:13:01 -0800

    Completed Form.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+
+/**
+ * Create state variables to hold tesm members
+ * and hold form values
+ * 
+ * Create update and submit functions
+ */
+
+  return (
+    <div className="App">
+      <h1>Team Builder App!!</h1>
+    </div>
+  );
+}
+
+export default App;
